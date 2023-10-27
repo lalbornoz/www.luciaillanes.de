7044a2e69b53ff5e23c28213f4a1aa8db5cd02c0
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2023-10-27 14:52:30 +0200

    assets/kades/: added. includes/{de,en,es}/etc.html: updated. assets/functions.js: updated. etc/kade13.png: deleted.

--- assets/functions.js
+++ assets/functions.js
@@ -1,4 +1,5 @@
 let catsClicked = false;
+let kadeList = ["kade9.png", "kade11.png", "kade13.png", "kade14.png", "kade19.png", "kade20.png", "kade23.png", "kade24.png", "kade25.png", "kade27.png", "kade29.png", "kade30.png", "kade32.png", "kade33.png", "kade35.png", "kade38.png", "kade42.png", "kade44.png", "kade52.png", "kade58.png", "kade59.png", "kade65.png", "kade73.png", "kade81.png", "kade86.png", "kade93.png"];
 
 function catsClick() {
 	if (catsClicked) {
@@ -7,5 +8,22 @@ function catsClick() {
 	} else {
 		document.body.style = '-moz-transform: rotate(180deg)';
 		catsClicked = true;
-	}
-}
+	};
+};
+
+function kadeClick() {
+	let kadeElement = document.getElementById("kade");
+	let kadeCurrent = kadeElement.src;
+	let kadeLastSepIdx = kadeCurrent.lastIndexOf("/");
+	let kadePathBase = kadeCurrent.substring(0, ((kadeLastSepIdx == -1) ? kadeCurrent.length : kadeLastSepIdx));
+	kadeCurrent = kadeCurrent.substring(((kadeLastSepIdx == -1) ? 0 : (kadeLastSepIdx + 1)));
+
+	while (true) {
+		let kadeNextIdx = (Math.random() * (kadeList.length ? (kadeList.length - 1) : 0)).toFixed();
+		let kadeNext = kadeList[kadeNextIdx];
+		if (kadeNext != kadeCurrent) {
+			kadeElement.src = kadePathBase + "/" + kadeNext;
+			break;
+		};
+	};
+};
