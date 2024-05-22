50303d8990f9cbd912f7ac3738e7188469fd002a
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2024-05-22 09:04:15 +0200

    assets/functions.js: updated.

--- assets/functions.js
+++ assets/functions.js
@@ -26,7 +26,7 @@ function catsClick() {
 		document.body.style = '';
 		catsClicked = false;
 	} else {
-		document.body.style = '-moz-transform: rotate(180deg)';
+		document.body.style = 'transform: rotate(180deg)';
 		catsClicked = true;
 	};
 };
