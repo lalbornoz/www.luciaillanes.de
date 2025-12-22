5ec70baeb364f480e80371d3c9368c66eb946fac
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2025-12-22 05:38:08 +0100

    Update personal description blob at top of page.

--- includes/de/legend.php
+++ includes/de/legend.php
@@ -1,7 +1,7 @@
     <div class="div-box div-legend">
       Willkommen auf der persönlichen Webseite von<br>
       <h1>Lucía Andrea Illanes Albornoz</h1><br>
-      <h2>Systemprogrammiererin | Systems engineer</h2><br>
+      <h2>Mehrheitlich eukaryotische multizelluläre Lebensform</h2><br>
       <h3
           title="Nin me šara u dalla ea&#10;Lady of the innumerable cosmic powers, resplendent daylight&#10;(incipit of the Sumeroakkadian exaltation of Inanna-Ištar)">
         🏳️‍⚧️ 𒊩 𒈨 𒊬𒊏 𒌓 𒁲𒆷 𒂊𒀀 🏳️‍⚧️
