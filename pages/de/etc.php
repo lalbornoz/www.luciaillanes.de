bb89796e6dae93b19bb1e735d1bd1ed91a1f9df4
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2025-12-22 13:46:06 +0100

    Add invocation from the Gilgameš to Et cetera page.

--- pages/de/etc.php
+++ pages/de/etc.php
@@ -25,7 +25,13 @@
         <div class="div-box div-content-etc-kade">
           <img alt="kade" class="div-content-etc-img" id="kade" onclick="kadeClick();" src="/assets/kades/kade13.png" title="Klick mich nicht!">
           <span id="kade-prev" onclick="kadePrevious();" style="cursor: pointer">⏪</span>
-          <span id="kade-next" onclick="kadeNext();" style="cursor: pointer">⏩</span>
+          <span id="kade-next" onclick="kadeNext();" style="cursor: pointer">⏩</span><br>
+          <span style="font-size: 1em; text-align: center"
+                title="Ana ešrā bēr iksupū kusāpa&#10;Ana šalāšā bēr iškunū nubatta&#10;Ikšudūnim-ma ana libbi Uruk supūri&#10;At twenty leagues they broke bread,&#10;at thirty leagues they pitched camp.&#10;They arrived in Uruk the Sheepfold.&#10;(from the Standard Babylonian text of The Epic of Gilgameš, Tablet XI)">
+            𒀀𒈾𒂊𒊭𒊏𒀀𒁁𒂊𒊑𒅅𒅖𒌑𒅆𒆪𒋢𒀀𒉺
+            𒀀𒈾𒊭𒆷𒀀𒊭𒀀𒁁𒂊𒊑𒅖𒆪𒉡𒌑𒉡𒁉𒋫
+            𒅅𒊭𒌑𒁺𒌑𒉡𒈠𒀀𒈾𒇷𒁉𒌷𒀕𒆠𒋢𒌑𒊑
+          </span><br>
         </div>
 
         <div class="div-box div-content-etc-german">
