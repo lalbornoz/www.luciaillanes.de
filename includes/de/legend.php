a174590f360dc30a698781fe4805b878c8eac85a
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2025-12-27 18:50:02 +0100

    Reorganise pages layout at top of page.

--- includes/de/legend.php
+++ includes/de/legend.php
@@ -26,9 +26,9 @@
       <span class="span-menu">
         <a href="/<?php echo $inc_lang ?>/arabic_poems">Arabische Gedichte</a> |
         <a href="/<?php echo $inc_lang ?>/stories">Unvollendete Träume</a><br>
-        <a href="/<?php echo $inc_lang ?>/movieslist">Filmbewertungen</a><br>
         <a href="/<?php echo $inc_lang ?>/araboly">Araboly</a> |
-        <a href="/<?php echo $inc_lang ?>/visual_sundries">Visuelle Grabbelkiste</a> |
+        <a href="/<?php echo $inc_lang ?>/visual_sundries">Visuelle Grabbelkiste</a><br>
+        <a href="/<?php echo $inc_lang ?>/movieslist">Filmbewertungen</a> |
         <a href="/<?php echo $inc_lang ?>/etc">Et cetera</a>
       </span>
       <hr>
