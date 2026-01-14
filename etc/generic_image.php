7fc20834dc6d7fd6abcd429cc0764ac50f0f098f
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2026-01-14 15:41:03 +0100

    Adds "Innenleben einer Katze" to etc page.

---
+++ etc/generic_image.php
@@ -0,0 +1,10 @@
+      <div class="div-box div-content" style="font-family: times new roman">
+        <img
+          alt="<?php echo $inc_uri_fname ?>"
+          src="<?php echo $inc_uri_fname ?>.png"
+          style="display: block; margin-left: auto; margin-right: auto; max-height: 100%; max-width: 100%">
+      </div>
+
+  <!--
+    vim:ts=2 sw=2 expandtab fenc=utf-8 foldmethod=marker nowrap tw=0
+    -->
