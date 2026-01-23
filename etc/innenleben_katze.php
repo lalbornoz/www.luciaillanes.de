a71ee3c28f1d23d63b4a095296ef7c6e37be550c
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2026-01-23 16:48:36 +0100

    Indentation in content files, pt. II.

--- etc/innenleben_katze.php
+++ etc/innenleben_katze.php
@@ -1,10 +1,1 @@
-      <div class="div-box div-content">
-        <img
-          alt="<?php echo $inc_uri_fname ?>"
-          src="<?php echo $inc_uri_fname ?>.png"
-          id="img-generic-image">
-      </div>
-
-  <!--
-    vim:ts=2 sw=2 expandtab fenc=utf-8 foldmethod=marker nowrap tw=0
-    -->
+../generic_image.php
