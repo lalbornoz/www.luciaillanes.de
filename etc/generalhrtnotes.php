284ad3db448241e092472abb9960c7a411cd0958
Lucía Andrea Illanes Albornoz <lucia@luciaillanes.de>
2026-06-12 09:35:15 +0200

    Adds injection protocol section to /etc/generalhrtnotes.

--- etc/generalhrtnotes.php
+++ etc/generalhrtnotes.php
@@ -30,6 +30,7 @@
         <li><a href="#hrt-transdermal-steady-state">Transdermal Oestradiol gel: steady state</a></li>
         <li><a href="#hrt-general-thyroid">General: acute thyroid downregulation</a></li>
         <li><a href="#hrt-general-hypophysis">General: familial pattern of hypophyseal dysregulation</a></li>
+        <li><a href="#hrt-general-injection">General: injection protocol</a></li>
         <li><a href="#hrt-references">References</a></li>
       </ol>
 
@@ -285,6 +286,85 @@
                 particular wrt. the above mentioned hypothyroidic syndrome.
               </li>
             </ol>
+          </li>
+        </ol>
+      </p>
+
+      <h2 class="text-centre" id="hrt-general-injection">General: injection protocol</h2>
+
+      <p>
+        This injection protocol employs subcutaneous injection as route of administration and utilises disposable insulin syringes with
+        a length of 8mm, a needle gauge size of 30G, a maximum injection volume of 0.3 mL divided up into 30 units of 0.01 mL each, and
+        a target injection volume of 0.1 mL, e.g. 10 bars.<br>
+        <ol>
+          <li>
+            Be at ease. Injections are fun!
+          </li>
+          <li>
+            Choose an injection site from a set of rotating injection sites. The upper outer thigh, given sufficient subcutaneous adipose
+            tissue when pinching, allows for a convenient injection at a 90° angle whilst sat.
+          </li>
+          <li>
+            Wash hands, ward against UV light sources (e.g. draw curtains,) and verify integrity and intactness of vial (the vial itself,
+            crimp seal, rubber stopper) and oil (no discolouration, cloudiness, separation, suspended particles, etc.; crystallisation induced
+            by cold temperatures can be dissolved by agitation)
+          </li>
+          <li>
+            Retrieve syringe container and syringe, remove plunger cap, and draw in a volume of air corresponding to the volume of oil to
+            be injected.
+          </li>
+          <li>
+            Swab the top of the vial, primarily the rubber stopper, with a 70% isopropyl alcohol swab for 30 seconds to 1 minute, wash hands,
+            swab the injection site with a 70% isopropyl alcohol swab for 30 seconds to 1 minute, wash hands.
+          </li>
+          <li>
+            Issue the appropriate invocations and prayers to a deity of your choice, such as Ištar the Lioness or Enki, the Ea, Lord Sea, in
+            order to avert the remotest possibility of "coring" or spontaneous vial existence failure through Neutrino bursts. Staring at empty
+            space and contemplating all-surrounding nothingness also works.
+          </li>
+          <li>
+            Remove syringe needle cap, place vial on stable surface, hold vial steadily with non-dominant hand, and, with the dominant hand,
+            steadily fully insert the needle into the centre of the rubber stopper. Any significant, especially lateral, movement that could
+            result in stretching of the canal inside the rubber stopper made in the process is to be avoided, even if the likelihood of "coring"
+            with a 30G needle is low.
+          </li>
+          <li>
+            Whilst steadily holding both the vial as well as the syringe, flip both upside down in one steady movement, draw oil from the vial
+            by pulling the plunger down - try not to touch the actual barrel itself - to the appropriate position, and wait until the target
+            injection volume has been attained. Given any air bubbles, push them back into the vial by pushing the plunger as appropriately
+            and pulling the plunger back down to the appropriate position, etc. pp.
+          </li>
+          <li>
+            Steadily rotate the vial as well as syringe back to their original position, remove the syringe, if necessary: wipe the top of
+            the rubber stopper, and stash the vial.
+          </li>
+          <li>
+            Pinch a fold of skin at the injection site between thumb and index finger with a separation of at least about an inch, hold,
+            insert needle, and release fold. Ensure that the syringe after releasing the fold and during the following steps until removal
+            is pushed into and below the skin to an appropriate degree and not held too loosely.
+          </li>
+          <li>
+            Perform injection by slowly pushing down on the plunger - try not to touch the actual barrel itself - across a time period of
+            up to a few minutes; the slower the rate of injection, the lower the risk of leakage. Pay close attention to motor feedback,
+            muscle memory, inasmuch as visual feedback.
+          </li>
+          <li>
+            Whilst maintaining the syringe in its position, wait for about 30 seconds to 1 minute in order to additionally lower the risk
+            of leakage.
+          </li>
+          <li>
+            Remove syringe, apply cotton swab (sterility is not required) to injection site in case of leakage or - rarely - miniscule
+            amounts of blood if a vein was sliced, etc. Undesirable reactions, e.g. itchiness, swelling, inflammation, etc. to Benzyl
+            Benzoate at/below the injection site have been documented in some people and may or may not occur and should subside relatively
+            quickly, if they do appear; topical antihistamines are documented to be effective in case of a prolonged reaction.<br>
+            <br>
+            In case of a much more significant and prolonged local let alone systemic reaction, such as infection and fever, direct
+            yourself to your nearest ER - note that this should under no circumstances ever actually occur unless this protocol was
+            improperly followed wrt. sterilisation and/or in case of contaminated oil, be it before it was ever used or at any point
+            thereafter.
+          </li>
+          <li>
+            Put syringe into syringe container and close lid.
           </li>
         </ol>
       </p>
